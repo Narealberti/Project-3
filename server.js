ae6837ea723cfec99b1f1221fb35a398bad4e176
nunu <41040001+Narealberti@users.noreply.github.com>
2019-02-20 13:26:11 -0800

    trying again

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/orders', orders);
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(client / build));
+    app.use(express.static("client / build"));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
